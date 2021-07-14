25566b5df660b852f62f3774b747f8c4911ab6f3
foysal ahmed <mdrad43@gmail.com>
2021-07-14 21:12:43 +0800

    added a button tag which will create checked and delate button

--- js/app.js
+++ js/app.js
@@ -11,4 +11,31 @@ function addTodo(event) {
   // prevent form from submitting
 
   event.preventDefault();
+  // add div tag
+
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  // create li element
+
+  const todoLi = document.createElement("li");
+  // todoLi.classList.add("todo-item");
+  // todoLi.innerText = "my name is khan";
+  todoLi.classList.add("todo-item");
+  todoDiv.appendChild(todoLi);
+
+  // add checked mark button
+
+  const checkedBtn = document.createElement("button");
+  checkedBtn.innerHTML = '<i class="fas fa-check"></i>';
+  checkedBtn.classList.add("checked");
+  todoDiv.appendChild(checkedBtn);
+
+  // delate checkedBtn
+
+  const delateBtn = document.createElement("button");
+  delateBtn.innerHTML = '<i class="fas fa-trash"></i>';
+  delateBtn.classList.add("delated");
+  todoDiv.appendChild(delateBtn);
+  // append todoList
+  todoList.appendChild(todoDiv);
 }
--- js/dist/app.dev.js
+++ js/dist/app.dev.js
@@ -9,5 +9,26 @@ todoBtn.addEventListener("click", addTodo); // Function
 
 function addTodo(event) {
   // prevent form from submitting
-  event.preventDefault();
+  event.preventDefault(); // add div tag
+
+  var todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo"); // create li element
+
+  var todoLi = document.createElement("li"); // todoLi.classList.add("todo-item");
+  // todoLi.innerText = "my name is khan";
+
+  todoLi.classList.add("todo-item");
+  todoDiv.appendChild(todoLi); // add checked mark button
+
+  var checkedBtn = document.createElement("button");
+  checkedBtn.innerHTML = '<i class="fas fa-check"></i>';
+  checkedBtn.classList.add("checked");
+  todoDiv.appendChild(checkedBtn); // delate checkedBtn
+
+  var delateBtn = document.createElement("button");
+  delateBtn.innerHTML = '<i class="fas fa-trash"></i>';
+  delateBtn.classList.add("delated");
+  todoDiv.appendChild(delateBtn); // append todoList
+
+  todoList.appendChild(todoDiv);
 }
